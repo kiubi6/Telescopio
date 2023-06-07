1a92528ec0d26be9f7a54df67a0c59d1b3e7c671
kiubi6 <135581287+kiubi6@users.noreply.github.com>
2023-06-06 22:13:46 -0600

    Delete js directory

--- js/calcularStem.js
+++
@@ -1,7 +0,0 @@
-function calcula_periodo() {
-    const G = 6.67 * Math.pow(10, -11);
-    const M = 5.97 * Math.pow(10, 24);
-    const r = (6378 + 593) * 1000; // en metros
-    const periodo = 2 * Math.PI * Math.sqrt(Math.pow(r, 3) / (G * M));
-    document.getElementById('resultadoA').innerText = 'El periodo orbital del telescopio Hubble es ' + periodo + ' segundos.';
-}
